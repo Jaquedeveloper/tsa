7dd6d537b56e5c2af544228ab52bd4fa18ff437f
Denys Gaidukov <gaidukov.denis@gmail.com>
2015-02-01 20:17:39 +0200

    Little changes

--- install/celery_start.sh
+++ install/celery_start.sh
@@ -4,4 +4,4 @@ DJANGODIR=/webapps/tsa/tsa              # Django project directory
 # Activate the virtual environment
 cd $DJANGODIR
 source ../bin/activate
-celery --app=tsa.celery_app:app worker --loglevel=INFO
+exec celery --app=tsa.celery_app:app worker --loglevel=INFO
