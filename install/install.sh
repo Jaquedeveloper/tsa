5b865134ede32d14c9d131c05cee2043be4caf3a
Denys Gaidukov <joker-ace@users.noreply.github.com>
2015-02-01 19:58:01 +0200

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -4,7 +4,7 @@ cd ~
 cp /webapps/tsa/tsa/install/gunicorn_start.sh /webapps/tsa/bin/gunicorn_start.sh
 chmod +x /webapps/tsa/bin/gunicorn_start.sh
 cp /webapps/tsa/tsa/install/celery_start.sh /webapps/tsa/bin/celery_start.sh
-chmod +x /webapps/tsa/bin/celery_start.shh
+chmod +x /webapps/tsa/bin/celery_start.sh
 cp /webapps/tsa/tsa/install/tsa.supervisor.conf /etc/supervisor/conf.d/tsa.supervisor.conf
 cp /webapps/tsa/tsa/install/tsa_celery.supervisor.conf /etc/supervisor/conf.d/tsa_celery.supervisor.conf
 cp /webapps/tsa/tsa/install/tsa.nginx.conf /etc/nginx/sites-available/tsa.nginx.conf
